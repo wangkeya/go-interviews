b7ef17183611b81bb99317fcbc22a80236dc36ea
wangkeya <783242528@qq.com>
2022-06-28 15:39:31 +0800

    修改gomod为1.18

--- generic/main.go
+++ generic/main.go
@@ -2,12 +2,26 @@ package main
 
 import "fmt"
 
-func Print[T interface{}](s ...T) {
+func Print[T any](s ...T) {
 	for _, v := range s {
 		fmt.Print(v)
 	}
 }
 
+func Equal[E comparable](s1, s2 []E) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i, v1 := range s1 {
+		v2 := s2[i]
+		if v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	Print("Hello, ", "playground\n")
+	Print(Equal([]string{"1"}, []string{"1"}))
 }
