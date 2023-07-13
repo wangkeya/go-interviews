34db027af7a038d1abbd3d2ee13db5f489377e36
wangkeya <783242528@qq.com>
2023-07-13 11:32:11 +0800

    map遍历无序

--- map_loop/main.go
+++ map_loop/main.go
@@ -6,6 +6,11 @@ import "fmt"
 func main() {
 	ageMp := make(map[string]int)
 	ageMp["qcrao"] = 18
+	ageMp["qhi"] = 20
+	ageMp["qbac"] = 90
+	ageMp["qbae"] = 90
+	ageMp["qbad"] = 90
+	ageMp["qbag"] = 90
 
 	for name, age := range ageMp {
 		fmt.Println(name, age)
